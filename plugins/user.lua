d4fd6368ab8de5b0f307f98b03d5b1d79878b1dc
Hudson Bruno <hb.natal@gmail.com>
2024-02-01 22:31:39 -0300

    feat: add markdown preview

--- plugins/user.lua
+++ plugins/user.lua
@@ -13,4 +13,10 @@ return {
     'andweeb/presence.nvim',
     lazy = false,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 }
