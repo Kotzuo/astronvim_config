de6d111411672a5d8e0cc80dd133ddad78a0d993
Hudson Bruno <hb.natal@gmail.com>
2024-02-07 20:36:50 -0300

    feat: add todo-comments.nvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -21,4 +21,13 @@ return {
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  {
+    "folke/todo-comments.nvim",
+    keys = {
+      { "]T", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+      { "[T", function() require("todo-comments").jump_next() end, desc = "Previous todo comment" },
+      { "<leader>T", "<cmd>TodoTrouble<cr>", desc = "Show all todo's" },
+    },
+  },
 }
