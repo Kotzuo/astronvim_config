b6b4bcfabb737d887c51afd75492898f3a57878f
Hudson Bruno <hb.natal@gmail.com>
2024-02-09 22:57:12 -0300

    feat: add gen.nvim

---
+++ plugins/gen.lua
@@ -0,0 +1,10 @@
+return {
+  "David-Kunz/gen.nvim",
+  event = "User AstroFile",
+  opts = {
+    display_mode = "split",
+  },
+  keys = {
+    { "<leader>a", ":Gen<cr>", desc = "Open Ai", mode = { "v", "n" } },
+  },
+}
